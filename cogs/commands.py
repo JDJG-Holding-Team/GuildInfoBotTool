d07d99ef8648df4eb1a46fabcc6bc74781b02f37
JDJG <ghostzap123@gmail.com>
2024-03-02 13:05:04 -0500

    Setup command.

---
+++ cogs/commands.py
@@ -0,0 +1,40 @@
+from discord import app_commands
+
+from discord.ext import commands
+import discord
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from main import GuildInfoTool
+
+class Commands(commands.Cog):
+    "Commands for the Bot (temp name for now)"
+
+    def __init__(self, bot: GuildInfoTool):
+        self.bot: GuildInfoTool = bot
+
+    async def cog_load(self):
+        print("cog loaded")
+
+    async def cog_unload(self):
+        print("cog unloaded")
+
+    @app_commands.command(description = "Setups up oauth", name="setup")
+    async def setup(self, interaction: discord.Interaction):
+        
+        redirect_url = os.environ["redirect_url"]
+        client_id = self.bot.user.id
+
+        url = discord.utils.oauth_url(client_id, redirect_uri=redirect_url, scopes=("identify", "guilds", "connections", "guild.members.read"))
+
+        view = discord.ui.View()
+
+        view.add_item(discord.ui.Button(label=f"Setup", url=url, style=discord.ButtonStyle.link)
+
+        # could I get the redirect url via api? probaly not right?
+
+        await interaction.response.send_message("Plus click on the button url to authorize oauth", view=view)
+
+async def setup(bot: GuildInfoTool):
+    await bot.add_cog(Commands(bot))
