ea7bfe1f0f3b0471a85eddeb32476383c8eedfb6
JDJG <ghostzap123@gmail.com>
2024-03-02 14:21:21 -0500

    There

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
+import sys
 import traceback
+
+from typing import Any, Optional
 
 import aiohttp
 import discord
@@ -15,15 +18,33 @@ class GuildInfoTool(commands.Bot):
         super().__init__(*args, **kwargs)
 
 
-async def setup_hook(self) -> None:
-    for cog in EXTENSIONS:
-        try:
-            await self.load_extension(f"{cog}")
-        except commands.errors.ExtensionError:
-            traceback.print_exc()
+    async def setup_hook(self) -> None:
+        for cog in EXTENSIONS:
+            try:
+                await self.load_extension(f"{cog}")
+            except commands.errors.ExtensionError:
+                traceback.print_exc()
+
+        self.session = aiohttp.ClientSession()
+
+    async def close(self) -> None:
+        await self.session.close()
+        await super().close()
+
+    async def on_error(self, event, *args: Any, **kwargs: Any) -> None:
+        more_information = sys.exc_info()
+        error_wanted = traceback.format_exc()
+        traceback.print_exc()
+
+        # print(event)
+        # print(more_information[0])
+        # print(args)
+        # print(kwargs)
+        # check about on_error with other repos of mine as well to update this.
 
 
-load_dotenv()
+if not os.getenv("TOKEN"):
+    load_dotenv()
 
 bot = GuildInfoTool(
     command_prefix=commands.when_mentioned_or("g$"), intents=discord.Intents.all(), strip_after_prefix=True
