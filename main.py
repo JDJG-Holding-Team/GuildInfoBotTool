14bd63b47c9af93be60b4c3f4dca5c33ebb67b01
JDJG <ghostzap123@gmail.com>
2024-03-04 03:19:52 -0500

    There

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ class GuildInfoTool(commands.Bot):
 
         server.app["guild_data"] = self.guild_data
         self.runner = aiohttp.web.AppRunner(server.app)
-        await asyncio.create_task(self.runner.setup())
+        asyncio.create_task(self.runner.setup())
         self.site = aiohttp.web.TCPSite(self.runner, host="localhost", port=2343)
-        await asyncio.create_task(self.site.start())
+        asyncio.create_task(self.site.start())
 
     async def close(self) -> None:
         await self.session.close()
