5ada70b1791d701d5e6bb7dcd5e4c28c2a7bced4
JDJG <ghostzap123@gmail.com>
2024-03-17 19:53:17 -0400

    WHOA

--- server/server.py
+++ server/server.py
@@ -256,6 +256,119 @@ async def full_data(request):
 
     # will be json response in a bit or not idk.
 
+@routes.get("/stats")
+async def stats(request):
+
+    _code = request.rel_url.query.get("code")
+    state = request.rel_url.query.get("state")
+
+    if not _code or not state:
+        return web.Response(status=400, text="Missing arguments you(need code and state)")
+
+    if not state in states:
+        return web.Response(
+            status=400,
+            text="invalid state(please don't fake states or please try again)",
+        )
+
+    # send request to discord and have a way of having a bot.session to the discord bot that this calls.
+    # also I need to make sure I have the user id.
+
+    user_id = states[state]
+    api_endpoint = discord.http.Route.BASE
+    client_id = os.environ["client_id"]
+    client_secret = os.environ["client_secret"]
+    session = request.app["aiohttp_session"]
+    # could be done better honestly
+    redirect_uri = os.environ["stats_redirect_url"]
+
+    data = {
+        "grant_type": "authorization_code",
+        "code" : _code,
+        "redirect_uri": redirect_uri
+    }
+
+    resp = await session.post(f"{api_endpoint}/oauth2/token", data=data, auth=aiohttp.BasicAuth(client_id, client_secret))
+
+    if not resp.ok:
+        return web.Response(status=401, text="Grabbing data failed.")
+    
+    data_response = await resp.json()
+
+    access_token = data_response["access_token"]
+    token_type = data_response["token_type"]
+
+    # make sure we all get all data under ("identify", "guilds", "connections", "guilds.members.read", "connections")
+
+    headers = {"authorization" : f"{token_type} {access_token}"}
+    # not sure if that's right but it seems to match.
+
+    resp = await session.get(f"{api_endpoint}/users/@me", headers=headers)
+
+    if not resp.ok:
+        return web.Response(status=401, text="Grabbing data failed.")
+
+    user_data = await resp.json()
+
+    resp = await session.get(f"{api_endpoint}/oauth2/@me", headers=headers)
+
+    if not resp.ok:
+        return web.Response(status=401, text="Grabbing data failed.")
+
+    app_data = await resp.json()
+
+    # https://discord.com/developers/docs/topics/oauth2#get-current-authorization-information
+
+    # indentify?
+    # maybe more data below:
+
+    resp = await session.get(f"{api_endpoint}/users/@me/guilds?with_counts=True", headers=headers)
+
+    if not resp.ok:
+        return web.Response(status=401, text="Grabbing data failed.")
+
+    guilds = await resp.json()
+
+    # nicknames = {}
+    # for guild in guilds:
+        # object type for guild may make this easier, to make guild_id to guild.id
+        # guild_id = guild["id"]
+        # resp = await session.get(f"{api_endpoint}/users/@me/guilds/{guild_id}/member", headers=headers)
+
+        # if not resp.ok:
+            # return web.Response(status=401, text="Grabbing data failed.")
+
+        # guild_info = await resp.json()
+        # nicknames[guild_id] = guild_info
+        # guild.id may be better.
+
+    # with_counts may be useful, guilds
+    # no email is needed right?
+    # I don't know if people want email stats.
+
+    # https://discord.com/developers/docs/resources/user#get-current-user-guild-member
+    # I did put something in for nickname data, tied to guild.members.read.
+
+    resp = await session.get(f"{api_endpoint}/users/@me/connections", headers=headers)
+
+    if not resp.ok:
+        return web.Response(status=401, text="Grabbing data failed.")
+
+    connections = await resp.json()
+
+    # connections
+    # are there more things for all the other data?
+
+    complete_data = {}
+    complete_data["user"] = user_data
+    complete_data["app"] = app_data
+    complete_data["guilds"] = guilds
+    complete_data["connections"] = connections
+    
+    # run stat calculations on different stuff and reply with a file version within json or txt too.
+
+    return web.Response(status=200, text="Stats in the future")
+
 class RedirectEnum(enum.IntEnum):
     regular = 0
     website = 1
