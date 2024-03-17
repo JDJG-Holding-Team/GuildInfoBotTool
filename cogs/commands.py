412123a1bc40cfec062a1c0153273b07f833376d
JDJG <ghostzap123@gmail.com>
2024-03-17 17:48:53 -0400

    There

--- cogs/commands.py
+++ cogs/commands.py
@@ -99,6 +99,25 @@ class Commands(commands.Cog):
         del self.bot.guild_data[interaction.user.id]
         # add some validation will not sync in till case.
 
+    @app_commands.command(description="Setups up oauth but sends data only to the site", name="site_setup")
+    async def site_setup(self, interaction: discord.Interaction):
+
+        params = {"client_id": self.bot.user.id, "user_id": interaction.user.id, "redirect_int" : 1}
+        url = URL("http://localhost:3000/generate-url")
+        full_url = url.with_query(params)
+
+        resp = await self.bot.session.get(full_url)
+        
+        data = await resp.json()
+
+        redirect_url = data["url"]
+
+        view = discord.ui.View()
+
+        view.add_item(discord.ui.Button(label=f"Setup", url=redirect_url, style=discord.ButtonStyle.link))
+
+        await interaction.response.send_message("Please Click on the button url to authorize oauth", view=view)
+
 
 async def setup(bot: GuildInfoTool):
     await bot.add_cog(Commands(bot))
