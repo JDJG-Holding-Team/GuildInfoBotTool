b5f85e0ee9ed20c6fb80a7b9432b15939b03a426
JDJG <ghostzap123@gmail.com>
2024-03-17 22:47:04 -0400

    There

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-from .extra import RedirectEnum
+from .extra import RedirectEnum, stats_builder
 from .server import handle_basic_response
 from .views import BasicButtons
--- utils/extra.py
+++ utils/extra.py
@@ -1,7 +1,26 @@
 import enum
 
+import discord
+
 
 class RedirectEnum(enum.IntEnum):
     regular = 0
     website = 1
     stats = 2
+
+def stats_builder(data : dict):
+
+    user = data["user"]
+    app_data = data["app"]
+    # unsure if I will need app_data for this.
+    
+    guilds = data["guilds"]
+    # this I can make stats like this without having to rely on their work:
+    # to replace https://discordtools.io/guildcount
+    # https://dinfo.fumple.pl/
+    # https://discordlookup.com/guildlist
+    # https://discordlookup.com/experiments
+
+    connections = data["connections"]
+    # unknown for connection stats.
+    nicknames = data["nicknames"]
