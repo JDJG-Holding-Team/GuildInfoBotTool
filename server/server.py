48e3aa88d2c1818f87140b7482e0e69589f6c18e
JDJG <ghostzap123@gmail.com>
2024-03-17 20:26:32 -0400

    hold on.

--- server/server.py
+++ server/server.py
@@ -138,98 +138,11 @@ async def code(request):
 @routes.get("/full-data")
 async def full_data(request):
 
-    user_id = states[state]
-    api_endpoint = discord.http.Route.BASE
-    client_id = os.environ["client_id"]
-    client_secret = os.environ["client_secret"]
-    session = request.app["aiohttp_session"]
-    # could be done better honestly
-    redirect_uri = os.environ["website_redirect_url"]
+    redirect_uri = ""
+    await handle_basic_response(request, states, redirect_uri)
 
-    data = {
-        "grant_type": "authorization_code",
-        "code" : _code,
-        "redirect_uri": redirect_uri
-    }
-
-    resp = await session.post(f"{api_endpoint}/oauth2/token", data=data, auth=aiohttp.BasicAuth(client_id, client_secret))
-
-    if not resp.ok:
-        return web.Response(status=401, text="Grabbing data failed.")
-    
-    data_response = await resp.json()
-
-    access_token = data_response["access_token"]
-    token_type = data_response["token_type"]
-
-    # make sure we all get all data under ("identify", "guilds", "connections", "guilds.members.read", "connections")
-
-    headers = {"authorization" : f"{token_type} {access_token}"}
-    # not sure if that's right but it seems to match.
-
-    resp = await session.get(f"{api_endpoint}/users/@me", headers=headers)
-
-    if not resp.ok:
-        return web.Response(status=401, text="Grabbing data failed.")
-
-    user_data = await resp.json()
-
-    resp = await session.get(f"{api_endpoint}/oauth2/@me", headers=headers)
-
-    if not resp.ok:
-        return web.Response(status=401, text="Grabbing data failed.")
-
-    app_data = await resp.json()
-
-    # https://discord.com/developers/docs/topics/oauth2#get-current-authorization-information
-
-    # indentify?
-    # maybe more data below:
-
-    resp = await session.get(f"{api_endpoint}/users/@me/guilds?with_counts=True", headers=headers)
-
-    if not resp.ok:
-        return web.Response(status=401, text="Grabbing data failed.")
-
-    guilds = await resp.json()
-
-    # nicknames = {}
-    # for guild in guilds:
-        # object type for guild may make this easier, to make guild_id to guild.id
-        # guild_id = guild["id"]
-        # resp = await session.get(f"{api_endpoint}/users/@me/guilds/{guild_id}/member", headers=headers)
-
-        # if not resp.ok:
-            # return web.Response(status=401, text="Grabbing data failed.")
-
-        # guild_info = await resp.json()
-        # nicknames[guild_id] = guild_info
-        # guild.id may be better.
-
-    # with_counts may be useful, guilds
-    # no email is needed right?
-    # I don't know if people want email stats.
-
-    # https://discord.com/developers/docs/resources/user#get-current-user-guild-member
-    # I did put something in for nickname data, tied to guild.members.read.
-
-    resp = await session.get(f"{api_endpoint}/users/@me/connections", headers=headers)
-
-    if not resp.ok:
-        return web.Response(status=401, text="Grabbing data failed.")
-
-    connections = await resp.json()
-
-    # connections
-    # are there more things for all the other data?
-
-    complete_data = {}
-    complete_data["user"] = user_data
-    complete_data["app"] = app_data
-    complete_data["guilds"] = guilds
-    complete_data["connections"] = connections
-    # complete_data["nicknames"] = nicknames
-    # nicknames too many requests
+    if isinstance(data, web.Response):
+        return data
 
     json_string = json.dumps(data, indent=4)
     json_response = io.StringIO(json_string)
--- utils/server.py
+++ utils/server.py
@@ -1,6 +1,6 @@
 from aiohttp import web
 
-async def handle_basic_response(request : web.Request, ):
+async def handle_basic_response(request : web.Request, states : dict, redirect_uri : str):
     _code = request.rel_url.query.get("code")
     state = request.rel_url.query.get("state")
 
@@ -16,4 +16,96 @@ async def handle_basic_response(request : web.Request, ):
     # send request to discord and have a way of having a bot.session to the discord bot that this calls.
     # also I need to make sure I have the user id.
 
+    user_id = states[state]
+    api_endpoint = discord.http.Route.BASE
+    client_id = os.environ["client_id"]
+    client_secret = os.environ["client_secret"]
+    session = request.app["aiohttp_session"]
+    # could be done better honestly
+
+    data = {
+        "grant_type": "authorization_code",
+        "code" : _code,
+        "redirect_uri": redirect_uri
+    }
+
+    resp = await session.post(f"{api_endpoint}/oauth2/token", data=data, auth=aiohttp.BasicAuth(client_id, client_secret))
+
+    if not resp.ok:
+        return web.Response(status=401, text="Grabbing data failed.")
     
+    data_response = await resp.json()
+
+    access_token = data_response["access_token"]
+    token_type = data_response["token_type"]
+
+    # make sure we all get all data under ("identify", "guilds", "connections", "guilds.members.read", "connections")
+
+    headers = {"authorization" : f"{token_type} {access_token}"}
+    # not sure if that's right but it seems to match.
+
+    resp = await session.get(f"{api_endpoint}/users/@me", headers=headers)
+
+    if not resp.ok:
+        return web.Response(status=401, text="Grabbing data failed.")
+
+    user_data = await resp.json()
+
+    resp = await session.get(f"{api_endpoint}/oauth2/@me", headers=headers)
+
+    if not resp.ok:
+        return web.Response(status=401, text="Grabbing data failed.")
+
+    app_data = await resp.json()
+
+    # https://discord.com/developers/docs/topics/oauth2#get-current-authorization-information
+
+    # indentify?
+    # maybe more data below:
+
+    resp = await session.get(f"{api_endpoint}/users/@me/guilds?with_counts=True", headers=headers)
+
+    if not resp.ok:
+        return web.Response(status=401, text="Grabbing data failed.")
+
+    guilds = await resp.json()
+
+    # nicknames = {}
+    # for guild in guilds:
+        # object type for guild may make this easier, to make guild_id to guild.id
+        # guild_id = guild["id"]
+        # resp = await session.get(f"{api_endpoint}/users/@me/guilds/{guild_id}/member", headers=headers)
+
+        # if not resp.ok:
+            # return web.Response(status=401, text="Grabbing data failed.")
+
+        # guild_info = await resp.json()
+        # nicknames[guild_id] = guild_info
+        # guild.id may be better.
+
+    # with_counts may be useful, guilds
+    # no email is needed right?
+    # I don't know if people want email stats.
+
+    # https://discord.com/developers/docs/resources/user#get-current-user-guild-member
+    # I did put something in for nickname data, tied to guild.members.read.
+
+    resp = await session.get(f"{api_endpoint}/users/@me/connections", headers=headers)
+
+    if not resp.ok:
+        return web.Response(status=401, text="Grabbing data failed.")
+
+    connections = await resp.json()
+
+    # connections
+    # are there more things for all the other data?
+
+    complete_data = {}
+    complete_data["user"] = user_data
+    complete_data["app"] = app_data
+    complete_data["guilds"] = guilds
+    complete_data["connections"] = connections
+    # complete_data["nicknames"] = nicknames
+    # nicknames too many requests
+
+    return complete_data
