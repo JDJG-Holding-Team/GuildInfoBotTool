16450874c9d66658bd5889b68d56d3c2d0e44ab7
JDJG <ghostzap123@gmail.com>
2024-03-17 20:57:48 -0400

    There

--- utils/views.py
+++ utils/views.py
@@ -1,3 +1,33 @@
 import discord
 
 # stuff like BasicButtons for the clear data command in the bot.
+# this should probaly be rewritten and given a timeout behavior but I think it already handles this?
+
+class BasicButtons(discord.ui.View):
+    def __init__(self, ctx, **kwargs):
+        super().__init__(**kwargs)
+        self.ctx = ctx
+        self.value: str = None
+
+    @discord.ui.button(label="Accept", style=discord.ButtonStyle.success, emoji="✅")
+    async def accept(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.clear_items()
+        await interaction.response.edit_message(view=self)
+        self.value = True
+        self.stop()
+
+    @discord.ui.button(label="Deny", style=discord.ButtonStyle.danger, emoji="✖️")
+    async def denied(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.clear_items()
+        await interaction.response.edit_message(view=self)
+        self.value = False
+        self.stop()
+
+    async def interaction_check(self, interaction: discord.Interaction):
+        if self.ctx.author.id != interaction.user.id:
+            return await interaction.response.send_message(
+                content=f"You Can't Use that button, {self.ctx.author.mention} is the author of this message.",
+                ephemeral=True,
+            )
+
+        return True
