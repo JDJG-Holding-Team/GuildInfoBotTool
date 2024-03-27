de821dab9c645bf1737f4979a3fc43990e93eadd
JDJG <ghostzap123@gmail.com>
2024-03-27 13:03:28 -0400

    Updates it a tiny bit.

--- server/server_rewrite.py
+++ server/server_rewrite.py
@@ -1,16 +1,61 @@
-import typing
+import io
+import json
+import os
+import secrets
+
 
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 
 
 app = FastAPI()
-
+app.state.states = {}
 # will need to be ran properly through awaitable method or ipc.
 
 @app.get("/", response_class=PlainTextResponse)
 async def main():
     return "Welcome Please let the bot direct you to the right spots"
+
+@routes.get("/generate-url")
+async def generate_url(client_id: typing.Optional[typing.Union[int, str]] = None, user_id : typing.Optional[typing.Union[int, str]] = None, redirect_int: typing.Optional[typing.Union[int, str]] = None):
+    
+    if not client_id or not user_id:
+        return {"error" : "Missing arguments(client_id or user_id)"}
+    
+    if isinstance(client_id, str) or isinstance(user_id, str) or isinstance(user_id, str):
+        return {"error" : "Non int variables being used."}
+    
+    # should all be pythonic
+    # should already be int
+
+    # pass something to the .stats variable somehow.
+
+    state = secrets.token_urlsafe(32)
+    app.state.states[state] = user_id
+
+    redirect_enum = RedirectEnum(redirect_int)
+
+    match redirect_enum:
+        case RedirectEnum.regular:
+            redirect_url = os.environ["redirect_url"]
+
+        case RedirectEnum.website:
+            redirect_url = os.environ["website_redirect_url"]
+
+        case RedirectEnum.stats:
+            redirect_url = os.environ["stats_redirect_url"]
+
+    # this looks kind of funky ngl
+
+    url = discord.utils.oauth_url(
+        client_id,
+        redirect_uri=redirect_url,
+        scopes=("identify", "connections", "guilds", "guilds.members.read"),
+        state=state,
+    )
+
+    return {"url": url}
+    
 
 
 @app.get("/code", response_class=PlainTextResponse)
