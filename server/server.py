17016a306545800cd8e0b3781702b32eef7583dc
JDJG <ghostzap123@gmail.com>
2024-04-22 19:08:54 -0400

    There

--- server/server.py
+++ server/server.py
@@ -19,7 +19,7 @@ from utils import RedirectEnum
 async def lifespan(app: FastAPI):
     async with aiohttp.ClientSession() as app.state.session:
         app.state.states = {}
-        app.state.guild_data = Dict[int, dict] = {}
+        app.state.guild_data: Dict[int, dict] = {}
         # just easier to create the stats does not need to be awaited.
         yield  # probaly closes when it is done.
         print("clean aiohttp session")
